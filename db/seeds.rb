338d2374fbc885792e1aa2d4c172ec8a52846c3f
Santosh Kamat <kamatsantosh7194@gmail.com>
2020-05-12 22:21:06 +0530

    Message me app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"Santosh1",password:"password1") 
+User.create(username:"Santosh2",password:"password1") 
+User.create(username:"Santosh3",password:"password1") 
+User.create(username:"Santosh4",password:"password1") 
+User.create(username:"Santosh5",password:"password1") 
+User.create(username:"Santosh6",password:"password1") 
+User.create(username:"Santosh7",password:"password1") 
+User.create(username:"Santosh8",password:"password1") 
+User.create(username:"Santosh9",password:"password1") 
+
+Message.create(body:"Hi, This is the first message!!", user_id:9)
+Message.create(body:"Hi, This is the second message!!", user_id:User.last)
+Message.create(body:"Hi, This is the third message!!", user_id:7)
+Message.create(body:"Hi, This is the fourth message!!", user_id:3)
+Message.create(body:"Hi, This is the fifth message!!", user_id:User.first)
