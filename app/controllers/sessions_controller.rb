cc09b956e58431ecd58c3d736959faf8054f07d3
Santosh Kamat <kamatsantosh7194@gmail.com>
2020-05-15 18:27:49 +0530

    UI changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,39 @@
 class SessionsController < ApplicationController
 
+	before_action :logged_in_users, only: [:new,:create]
+
 	def new
 		
 	end
+
+	def create
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			flash[:notice] = "Welcome, #{user.username}!!"
+			session[:user_id] = user.id	
+			redirect_to root_path	
+		else
+			flash.now[:alert] = "Please enter valid credentials."
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = "Logged out successfully!"
+		redirect_to login_path
+	end
+
+	private
+
+	def logged_in_users
+		if logged_in?
+			flash[:alert] = "You are already logged in"
+			redirect_to root_path
+		end
+	end
+
+	
+
 
 end
